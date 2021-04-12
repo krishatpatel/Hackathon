97e580b3d0cac0ca974449ad83fb610f9917c4ae
krishapatel17 <69741619+krishapatel17@users.noreply.github.com>
2021-04-12 00:35:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function english() {
+  window.location = "english.html";
+}
+
+function spanish() {
+  window.location = "spanish.html";
+}
+
+
+function gujarati() {
+  window.location = "gujarati.html";
+}
